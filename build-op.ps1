3ed00c271513eac0ec5dd5c4446509c09a685ac9
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2022-04-08 18:47:58 +0200

    Updated builder

--- build-op.ps1
+++
@@ -1,8 +0,0 @@
-$compress = @{
-    Path = "./info.toml", "./src"
-    CompressionLevel = "Fastest"
-    DestinationPath = "./DecorationUnlocker.op"
-}
-Compress-Archive @compress -Force
-
-Write-Host("✅ Done!")
